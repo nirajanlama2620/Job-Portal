242e74b8285fb801cc1e1ca184e9d313251bd6f7
nirajanlama2620 <nirajanlama2620@gmail.com>
2026-06-26 08:21:08 +0545

    feat: complete job portal project

---
+++ Backend/README.md
@@ -0,0 +1,5 @@
+lamanirajan502_db_user
+aCC0uORDTrvvyxEw
+
+mongodb+srv://lamanirajan502_db_user:aCC0uORDTrvvyxEw@jobportal.yfaxx2b.mongodb.net/
+mongodb+srv://lamanirajan502_db_user:<db_password>@jobportal.yfaxx2b.mongodb.net/?appName=JobPortal
---
+++ Frontend/src/redux/store.js
@@ -0,0 +1,48 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import jobSlice from "./jobSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import companySlice from "./companySlice";
+import applicationSlice from "./applicationSlice";
+
+const safeStorage = storage && (storage.setItem ? storage : storage.default);
+// redux-persist was written using an older JavaScript module system (CommonJS).
+//  When you import it into a modern environment like Vite using standard syntax:
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: safeStorage,
+}
+
+const rootReducer = combineReducers({
+    auth:authSlice,
+    job:jobSlice,
+    company:companySlice,
+    application:applicationSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+export const persistor = persistStore(store);
+export default store;
